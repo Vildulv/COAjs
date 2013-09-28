4f89b5e525c0c92efbe9930d89595ad53403bc7b
Vildulv <vildulv@gmail.com>
2013-09-28 18:08:06 +0300

    Core Js File

---
+++ core.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hev
+ * Date: 28.09.13
+ * Time: 17:39
+ * To change this template use File | Settings | File Templates.
+ */
+console.log('kjkkj');
